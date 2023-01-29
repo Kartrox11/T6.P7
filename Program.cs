b75d12c8b66a9ab4532edfec92e19c5899fbee51
Evgeniy Voronow <Evgelion11@gmail.com>
2023-01-29 20:21:11 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace Т6.П7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+
+            //for (int i = 0; i <= 17; i++)
+            //{
+            //    Console.Write("@");
+            //}
+            //Console.ReadKey();
+
+            //Задание 2
+
+            //for (int i = 0; i <= 24; i++)
+            //{
+            //    Console.WriteLine("$");
+            //}
+            //Console.ReadKey();
+
+            //Задание 3
+            //for (int i = 0; i <= 3; i++)
+            //{
+            //    Console.WriteLine(" ");
+            //}
+            //for (int j = 0; j <= 19; j++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+            //Console.ReadKey();
+
+            //Задание 4
+
+            //for (int i = 0; i <= 17; i++)
+            //{
+
+            //    for (int j = 0; j <= 4; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("%");
+            //}
+            //Console.ReadKey();
+
+            //Задание 5
+
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    for (int j = 0; j <= 14; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine("");
+            //}
+            //Console.ReadKey();
+
+            //Задание 6
+
+            //for (int i = 0; i <= 3; i++)
+            //{
+            //    for (int j = 0; j <= 6; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+            //    Console.WriteLine("");
+            //    Console.WriteLine(" ");
+            //}
+            //Console.ReadKey();
+
+        }
+    }
+}
